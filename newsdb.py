2179aef00a367b95cadd563795ea928558c406df
Mohamed Samir <Mohamed_samir9696@hotmail.com>
2018-06-19 23:51:18 +0200

    Last update

--- newsdb.py
+++ newsdb.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import psycopg2
 
 # Connect to the database using connect function
@@ -5,53 +6,46 @@ conn = psycopg2.connect("dbname=news")
 db = conn.cursor()
 
 
-def get_popular_three_articles():
-    """Return most popular three articles."""
-    query = '''Select title,views from articles,reports
-               where articles.slug = reports.slug
-               order by views desc limit 3;'''
+def execute_query(query):
+    """Function To Execute Different Queries """
     db.execute(query)
     return db.fetchall()
 
 
-def get_most_popular_article_authors():
-    """Return most popular article authors."""
-    query = '''Select name,SUM(views) as views from
+query1 = '''Select title,views from articles,reports
+               where articles.slug = reports.slug
+               order by views desc limit 3;'''
+
+query2 = '''Select name,SUM(views) as views from
                authors,articles,reports where
                articles.author = authors.id And
                articles.slug = reports.slug group by
-               name order by views desc;'''
-    db.execute(query)
-    return db.fetchall()
+               name order by views desc; '''
 
-
-def get_day_with_more_request_errors():
-    """ Return the day with more request errors """
-    # use Conditional Expressions to count the results of the condition where status like anything begin with 404
-    query = '''Select to_char(time,'YYYY-MON-DD') as day,round((count(case when status
+query3 = '''Select to_char(time,'YYYY-MON-DD') as day,round((count(case when status
              like '404%' then 1 else Null End)::numeric/
              count(*)::numeric)*100,2)
-             as error_precentage from log group by day
-             order by error_precentage desc limit 1;'''
-    db.execute(query)
-    return db.fetchall()
+             as error_percentage from log group by day
+             order by error_percentage desc limit 1; '''
 
 
-print('Top Three Articles in all time:')
-for data in get_popular_three_articles():
-    print(str(data[0]), str(data[1])+" views") # data[0] => represent title coulmn , data[1] => represent views coulmn
-
-print("\n")
-
-print("most popular article authors:")
-for data in get_most_popular_article_authors():
-    print(str(data[0]), str(data[1]) + " views") # data[0] => represent name coulmn , data[1] => represent Sum of views coulmn
+print('What are the most popular three articles of all time ?')
+for data in execute_query(query1):
+    print str(data[0]), " ---- ", str(data[1]), " views"
+    # data[0] => represent title column , data[1] => represent views column
 
 print("\n")
 
-print("day with more errors")
-for data in get_day_with_more_request_errors():
-    print(str(data[0]), str(data[1]) + " % errors") # data[0] => represent day , data[1] => represent precentage of error
+print("Who are the most popular article authors of all time ?")
+for data in execute_query(query2):
+    print str(data[0]), " ---- ", str(data[1]), " views"
+    # data[0] => represent name column , data[1] => represent Sum of views column
 
+print("\n")
 
-    conn.close()
+print("On which days did more than 1% of requests lead to errors ?")
+for data in execute_query(query3):
+    print str(data[0]), " ---- ", str(data[1]), "% errors"
+    # data[0] => represent day , data[1] => represent percentage of error
+
+conn.close()
